18370bd33bf74f7b13d0900ea2f0546a7597b06d
Ashish-sps <Ashishkiran.softprodigy@gmail.com>
2022-09-04 03:27:09 +0530

    second commit

--- src/main/java/test/dockertest/DockerTestApplication.java
+++ src/main/java/test/dockertest/DockerTestApplication.java
@@ -3,11 +3,16 @@ package test.dockertest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class DockerTestApplication {
 
 	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(DockerTestApplication.class.getName());
+		logger.info("Main method executed=====================================>>>>>");
 		SpringApplication.run(DockerTestApplication.class, args);
+
 	}
 
 }
